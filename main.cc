1fd838158ebc6850d050c93f49b651c79864d620
brian-250 <brianmilian@csu.fullerton.edu>
2023-11-16 01:57:55 -0800

    Remove comments

--- main.cc
+++ main.cc
@@ -8,13 +8,6 @@ int main() {
     int* num_ptr;
     num_ptr = &x;
 
-    // Print memory address in two ways
-    // 1. Print dereferenced x value '&x = memory address'
-    // 2. Print num_ptr value 'num_ptr = memory address'
-    // ---------------------------------------------------
-    // Print int value in two ways
-    // 3. Print x value 'x = 5'
-    // 4. Print dereferenced num_ptr value '*num_ptr = 5'
     std::cout << "---------------------------\n";
     std::cout << "Memory Address\n";
     std::cout << "&x: " << &x << '\n';
